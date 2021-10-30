6b36c74c73f29c33466f1db1a6df120af4befd03
Artem <art.gromov@bk.ru>
2021-10-30 13:06:34 +0300

    Add edit entry

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,5 +20,5 @@ from blog.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('notes/', include('blog.urls')),
+    path('api/v1/', include('blog.urls')),
 ]
--- app/blog/urls.py
+++ app/blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import NoteView
+
+from .views import NoteView, NoteDetailView, NoteEditorView
 
 
 app_name = "blog"
 urlpatterns = [
-    path('', NoteView.as_view()),
+    path('notes/', NoteView.as_view(), name='notes'),
+    path('notes/<int:note_id>/', NoteDetailView.as_view(), name='note'),
+    path('notes/add/', NoteEditorView.as_view(), name='add'),
+    path('notes/<int:note_id>/save/', NoteEditorView.as_view(), name='save'),
 ]
